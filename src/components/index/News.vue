66a2a84f936ee3e6ab09114d5d467d8291d1c2eb
patire-tu <994418277@qq.com>
2017-11-10 17:43:28 +0800

    更新一些细节

--- src/components/index/News.vue
+++ src/components/index/News.vue
@@ -1,9 +1,31 @@
 <style scoped>
-
+  .wrapper{
+    margin: 50px 15%;
+  }
+  .news-wrapper{
+    margin-top: 50px;
+    margin-bottom: 50px;
+  }
 </style>
 
 <template>
-  <news></news>
+  <div class="wrapper">
+    <div class="row">
+      <div class="col-md-9">
+        <news class="news-wrapper"></news>
+        <news class="news-wrapper"></news>
+        <news class="news-wrapper"></news>
+        <news class="news-wrapper"></news>
+        <news class="news-wrapper"></news>
+        <news class="news-wrapper"></news>
+      </div>
+      <div class="col-md-3">
+        <news class="news-wrapper"></news>
+        <news class="news-wrapper"></news>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
