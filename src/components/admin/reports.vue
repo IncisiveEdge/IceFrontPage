522615c923183bc16efb392a66639a39a940f554
patire-tu <994418277@qq.com>
2017-11-13 23:23:07 +0800

    ueditor联调准备

--- src/components/admin/reports.vue
+++ src/components/admin/reports.vue
@@ -1,30 +1,54 @@
 <template>
-  <div id="test-info-wrapper">
-    <ie-info :template="dfInfo.template" :data="dfInfo.data"></ie-info>
-  </div>
+  <div ref="editor"></div>
 </template>
 <script>
-  //  import $ from 'jquery'
-  import {IEInfoObject} from '../../assets/IEInfo'
-  import ieInfo from '../partical/DataForm/index'
-  import {infoTplData, infoData} from '../../assets/simulate/demo-PersonInfo'
+  /* eslint-disable */
+  import '@/assets/plugins/ueditor/ueditor.config'
+  import '@/assets/plugins/ueditor/ueditor.all'
 
-  //  console.log(DFInfo)
   export default {
-    name: 'ExamplePersonSimpleInfo',
-    data () {
+    data() {
       return {
-        dfInfo: new IEInfoObject('dfInfo', this)
+        id: new Date().getTime() * Math.random() + ''
       }
     },
-    components: {
-      ieInfo
+    props: {
+      value: {
+        type: String,
+        default: null
+      },
+      config: {
+        type: Object,
+        default: {}
+      }
     },
-    mounted () {
-      this.dfInfo.render(infoTplData, infoData).then(() => {
-        console.log(this)
+    watch: {
+      value: function value(val, oldVal) {
+        this.editor = UE.getEditor(this.id, this.config)
+        if (val !== null) {
+          this.editor.setContent(val)
+        }
+      },
+    },
+    mounted() {
+      this.$nextTick(function f1() {
+        // 保证 this.$el 已经插入文档
+        this.$refs.editor.id = this.id
+        this.editor = UE.getEditor(this.id, this.config)
+
+        this.editor.ready(function f2() {
+          this.editor.setContent(this.value)
+
+          this.editor.addListener("contentChange", function () {
+            const wordCount = this.editor.getContentLength(true)
+            const content = this.editor.getContent()
+            const plainTxt = this.editor.getPlainTxt()
+            this.$emit('input', { wordCount: wordCount, content: content, plainTxt: plainTxt })
+          }.bind(this))
+
+          this.$emit('ready', this.editor)
+        }.bind(this))
       })
     }
   }
 </script>
-
--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -63,46 +63,8 @@
 
 <template>
   <div style="height: 100%">
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".nav-collpase">
-            <span class="sr-only">切换导航</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a href="/" class="navbar-brand">Ice创客基地(前台)</a>
-        </div>
-        <div class="navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="/">首页</a></li>
-            <li><a href="/news">新闻资讯</a></li>
-            <li><a href="#">课程体系</a></li>
-            <li><a href="#">营地赛事</a></li>
-            <li><a href="/products">产品教具</a></li>
-            <li><a href="#">关于ICE</a></li>
-            <li class="dropdown">
-              <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown">
-                预约联系
-                <b class="caret"></b>
-              </a>
-              <ul class="dropdown-menu dropdown-menu-default">
-                <li><a href="#">预约</a></li>
-                <li><a href="#">报名</a></li>
-                <li><a href="#">加盟咨询</a></li>
-                <li><a href="#">公众号</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <!--<div class="container">-->
-    <!--<div class="alert">-->
-    <!--<a href="#" class="close" data-dismiss="alert">&times;</a>-->
-    <!--</div>-->
-    <!--</div>-->
+    <i-nav></i-nav>
+
     <div id="container" style="margin-top: 50px; height:calc(100% - 50px);overflow-x: hidden; overflow-y: auto; background-color: #eaeaea">
       <router-view></router-view>
       <hr>
@@ -146,8 +108,12 @@
 </template>
 
 <script>
+  import INav from '../partical/Navigator/NavClassic'
   export default {
     name: 'Index',
+    components: {
+      INav
+    },
     computed: {
       user () {
         this.$store.commit('getSessionUser')
